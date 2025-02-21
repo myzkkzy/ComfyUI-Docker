c42df6741f5098f02aa37b4cfcdcdfd7af64bcaa
Kazuya MIYAZAKI <myzkkzy@gmail.com>
2025-02-21 10:43:45 +0900

    [fix] Fixed ComfyUI directory to be accessible to non-root users.

--- comfyui/entrypoint.sh
+++ comfyui/entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-if [ -z "$(ls -A /ComfyUI 2>/dev/null)" ]; then
-    git clone https://github.com/comfyanonymous/ComfyUI.git /ComfyUI
+COMFYUI_DIR="/home/${USERNAME}/ComfyUI"
+COMFYUI_MAIN_PY="${COMFYUI_DIR}/main.py"
+
+# If ComfyUI main.py does not exist, install it.
+if [ ! -f $COMFYUI_MAIN_PY ]; then
+    cd $COMFYUI_DIR
+    # If the file exists, git clone will not be possible, so if .gitkeep exists, delete it.
+    if [ -f "${COMFYUI_DIR}/.gitkeep" ]; then
+        rm "${COMFYUI_DIR}/.gitkeep"
+    fi
+
+    git clone https://github.com/comfyanonymous/ComfyUI.git $COMFYUI_DIR
+    pip install --user -r requirements.txt
+    # Also install ComfyUI Manager.
+    git clone https://github.com/ltdrdata/ComfyUI-Manager.git $COMFYUI_DIR/custom_nodes/ComfyUI-Manager
 fi
 
-cd /ComfyUI
-pip install -r requirements.txt
-
-if [ ! -d "/ComfyUI/custom_nodes/ComfyUI-Manager" ]; then
-    git clone https://github.com/ltdrdata/ComfyUI-Manager.git /ComfyUI/custom_nodes/ComfyUI-Manager
-fi
-
-python3 main.py --listen 0.0.0.0
+# RUN ComfyUI. 
+python3 $COMFYUI_MAIN_PY --listen 0.0.0.0
